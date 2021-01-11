ab19c8b94bd5c3ffe9f8d63c4896b6f04aecb873
nickj <nickj@2020stonks.localdomain>
2021-01-11 17:18:09 -0300

    added antdesign and created a basic form

--- pages/_app.js
+++ pages/_app.js
@@ -3,5 +3,5 @@ import '../styles/vars.css'
 import '../styles/global.css'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />
 }
